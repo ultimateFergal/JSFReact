aae748b0adbc7b7dd20b03ea77f486b0f41644f0
Fernando Galarcio <fernandogalarcio@P44-LUISGALARCIO.local>
2021-03-01 21:49:58 -0500

    find and render, turno into array

--- 12arrays-block-buster/js/search.js
+++ 12arrays-block-buster/js/search.js
@@ -7,12 +7,28 @@ search.addEventListener('submit', function(event) {
     event.preventDefault()
     const formData = new FormData(this)
     const title = formData.get('title')
-    const movies = filterByTitle(title)
-    render(movies)
+    // const movies = filterByTitle(title)
+    const query = formData.get('title')
+    const movies = searchMovie(query)
+    if (movies)
+        return render(movies)
+    return alert('No encontramos tu película')
 })
 
 function filterByTitle(title) {
     return movies.filter((movie) => {
         return movie.title.toLowerCase().includes(title.toLowerCase())
     })
+}
+
+function findById(id) {
+    return movies.find((movie) => {
+        return movie.id === parseInt(id, 10)
+    })
+}
+function searchMovie(query) {
+    if (isNaN(query)) {
+        return filterByTitle(query)
+    }
+    return [findById(query)]
 }
